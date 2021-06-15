982bee4e7ea26857ef3af7e65cb61bafdd571b03
Krishanus-2001 <krishanu21saini@gmail.com>
2021-06-15 13:15:39 +0530

    Add gitignore

--- shot.js
+++
@@ -1,9 +0,0 @@
-const screenshot = require('screenshot-desktop')
-const fs = require('fs')
-
-screenshot({format: 'png'}).then((img) => {
-  // img: Buffer filled with png goodness
-  fs.writeFileSync('./image.png', img)
-}).catch((err) => {
-  console.log(err)
-})
